0820c931761791c87f937426f1f175db73f6ad49
anami <anamicity@gmail.com>
2015-01-13 17:34:54 +0000

    Support for jQuery 1.5 and overhaul
    Added support for jQuery 1.5
    Added seconds
    Updated licence and readme

--- js/timepicki.js
+++ js/timepicki.js
@@ -6,24 +6,93 @@
 
 	$.fn.timepicki = function(options) {
 
-		var defaults = {
-			format_output: function(tim, mini, meri) {
-				return tim + " : " + mini + " : " + meri;
+	    var defaults = {
+            // format the output as a string.
+			format_output: function(tim, mini, secs, meri, show_seconds) {
+			    if (show_seconds) {
+			        return tim + ":" + mini + ":" + secs + " " + meri;
+			    }
+			    return tim + ":" + mini + " " + meri;
 			},
-			increase_direction: 'down',
-			custom_classes: ''
+		    // function to format the input - part of the settings 
+            // in case it needs to be overridden
+			format_input: function(input) {
+				// format the input value of the control that timepicker is attached to.
+				var d = new Date(),
+					today = new Date(d.getFullYear() + "/" + (d.getMonth()+1) + "/" + d.getDate() + " " + input),
+					time = [],
+					ti = today.getHours(),
+					mi = today.getMinutes(),
+					se = today.getSeconds(),
+					mer = "AM";
+				
+				if (12 < ti) {
+					ti -= 12;
+					mer = "PM";
+				}
+
+				if (ti === 0) { ti = 12; }
+
+				// return the time part as an array.
+				time.push(ti);
+				time.push(mi);
+				time.push(se);
+				time.push(mer);
+				return time;
+			},
+		    // 'control' to use the value of the input element 
+		    // 'current' to use the current time.
+            // 'preset' to use a preset time as specified by preset_start_time
+			start_time_origin: 'control', 
+            // preset time used when 'start_time_origin' is set to 'preset'
+			preset_start_time: undefined,
+            // show seconds in the control
+			show_seconds: true,
+            // the 'up' buttons increase the value - 'down' buttons to increase the value 
+			increase_direction: 'up',
+            // custom CSS classes 
+			custom_classes: '',
+	        // make the initial textbox readonly so not to invoke any on screen keyboards
+            no_keyboard: true
 		};
 
-		var settings = $.extend({}, defaults, options);
+	    var settings = $.extend({}, defaults, options);
+
+	    // check if the jQuery is older than 1.7
+        // when .on was introduced
+	    function checkForOldjQuery() {
+	        var ver_reg = /1\.(\d){1}\.\d/i,
+                result = ver_reg.exec($.fn.jquery);
+	        if (result) {
+	            if (Number(result[1]) < 7) {
+	                return true;
+	            }
+	        }
+
+	        // presume it is a newer jQuery
+	        return false;
+	    }
+        
+	    var isjQueryOld = checkForOldjQuery();
 
 		return this.each(function() {
 
-			var ele = $(this);
-			var ele_hei = ele.outerHeight();
-			var ele_lef = ele.position().left;
+		    var ele = $(this),
+			    ele_hei = ele.outerHeight(),
+			    ele_lef = ele.position().left;
+
 			ele_hei += 10;
-			$(ele).wrap("<div class='time_pick'>");
+			if (settings.show_seconds) {
+			    $(ele).wrap("<div class='time_pick with-seconds'>");
+			} else {
+			    $(ele).wrap("<div class='time_pick'>");
+			}
 			var ele_par = $(this).parents(".time_pick");
+
+		    // force the control not to use the onscreen keyboard
+			if (settings.no_keyboard) {
+			    ele.attr("readonly", "readonly");
+			}
 
 			// developer can specify which arrow makes the numbers go up or down
 			var top_arrow_button = (settings.increase_direction === 'down') ?
@@ -33,25 +102,36 @@
 				"<div class='next action-next'></div>" :
 				"<div class='next action-prev'></div>";
 
-			ele_par.append(
-				"<div class='timepicker_wrap " + settings.custom_classes + "'>" +
+			var time_markup = "<div class='timepicker_wrap " + settings.custom_classes + "'>" +
 					"<div class='arrow_top'></div>" +
 					"<div class='time'>" +
 						top_arrow_button +
-						"<div class='ti_tx'><input type='text'></div>" +
+						"<div class='ti_tx'></div>" +
 						bottom_arrow_button +
 					"</div>" +
 					"<div class='mins'>" +
 						top_arrow_button +
-						"<div class='mi_tx'><input type='text'></div>" +
+						"<div class='mi_tx'></div>" +
 						bottom_arrow_button +
-					"</div>" +
+					"</div>{{SECONDS}}" +
 					"<div class='meridian'>" +
 						top_arrow_button +
-						"<div class='mer_tx'><input type='text'></div>" +
+						"<div class='mer_tx'></div>" +
 						bottom_arrow_button +
 					"</div>" +
-				"</div>");
+				"</div>";
+
+			if (settings.show_seconds) {
+			    time_markup = time_markup.replace("{{SECONDS}}", "<div class='secs'>" +
+						top_arrow_button +
+						"<div class='se_tx'></div>" +
+						bottom_arrow_button +
+					"</div>");
+			} else {
+			    time_markup = time_markup.replace("{{SECONDS}}", "");
+			}
+
+			ele_par.append(time_markup);
 			var ele_next = $(this).next(".timepicker_wrap");
 			var ele_next_all_child = ele_next.find("div");
 			ele_next.css({
@@ -59,58 +139,129 @@
 				"left": ele_lef + "px"
 			});
 
-			// open or close time picker when clicking
-			$(document).on("click", function(event) {
-				if (!$(event.target).is(ele_next)) {
-					if (!$(event.target).is(ele)) {
-						set_value(event, !is_element_in_timepicki($(event.target)));
-					} else {
-						open_timepicki();
-					}
-				}
-			});
 
-			// open the modal when the user focuses on the input
-			ele.on('focus', open_timepicki);
 
-			// close the modal when the time picker loses keyboard focus
-			ele_par.find('input').on('blur', function() {
-				setTimeout(function() {
-					var focused_element = $(document.activeElement);
-					if (!is_element_in_timepicki(focused_element)) {
-						close_timepicki();
-					}
-				}, 0);
-			});
-
-			function is_element_in_timepicki(jquery_element) {
-				return $.contains(ele_par[0], jquery_element[0]) || ele_par.is(jquery_element);
+		    // open or close time picker when clicking
+			if (isjQueryOld) {
+			    //console.info("using old jquery click");
+			    $(document).click(handleDocumentClickOld);
+			} else {
+			    $(document).on("click", handleDocumentClick);
+			}
+			
+			/* document click event handler for jQuery >= 1.7 and 2 */
+			function handleDocumentClick(event) {
+			    if (!$(event.target).is(ele_next)) {
+			        if (!$(event.target).is(ele)) {
+			            set_value(event, !is_element_in_timepicki($(event.target)));
+			        } else {
+			            open_timepicki(event);
+			        }
+			    }
 			}
 
-			function set_value(event, close) {
-				// use input values to set the time
-				var tim = ele_next.find(".ti_tx input").val();
-				var mini = ele_next.find(".mi_tx input").val();
-				var meri = ele_next.find(".mer_tx input").val();
+			/* document click event handler for jQuery < 1.7 */
+			function handleDocumentClickOld(event) {
+                // check if the click is in a timepicker..
+			    if ($(event.target).parents().is(".time_pick")) {
+					
+					var parent = $(event.target).parents().find(".time_pick");
 
-				if (tim.length != 0 && mini.length != 0 && meri.length != 0) {
-					// store the value so we can set the initial value
-					// next time the picker is opened
-					ele.attr('data-timepicki-tim', tim);
-					ele.attr('data-timepicki-mini', mini);
-					ele.attr('data-timepicki-meri', meri);
+					// check if the element that was clicked is in the parent.
+					if ($.contains(parent[0], event.target)){
 
-					// set the formatted value
-					ele.val(settings.format_output(tim, mini, meri));
-				}
+						// check if the picker is already open
+						if (ele_next.is(":visible")) {
+							//console.info("picker is open");
 
-				if (close) {
+							// if the clicked element is the actual textbox 
+							if ($(event.target).is("input")) {
+								// keep the picker open.
+								set_value(event, false);
+							} else {
+								// check if the click landed on the control first
+								set_value(event, !$.contains(ele_next[0], event.target));	
+							}
+						} 
+					}
+				} else {
 					close_timepicki();
 				}
 			}
 
-			function open_timepicki() {
-				set_date(settings.start_time);
+		    // open the modal when the user focuses on the input
+			ele.focus(open_timepicki);
+
+			// close the modal when the time picker loses keyboard focus
+			// ele_par.find('input').on('blur', function() {
+			// 	setTimeout(function() {
+			// 		console.log("input onblur...");
+			// 		var focused_element = $(document.activeElement);
+			// 		console.log(focused_element);
+			// 		if (!is_element_in_timepicki(focused_element)) {
+			// 			close_timepicki();
+			// 		}
+			// 	}, 0);
+			// });
+
+			function is_element_in_timepicki(jquery_element) {
+			    var element_in = $.contains(ele_par[0], jquery_element[0]) || ele_par.is(jquery_element);
+			    console.log(element_in);
+			    return element_in;
+			}
+
+			function set_value(event, close) {
+				// use input values to set the time
+				var tim = ele_next.find(".ti_tx").text(),
+				    mini = ele_next.find(".mi_tx").text(),
+				    secs = ele_next.find(".se_tx").text(),
+				    meri = ele_next.find(".mer_tx").text();
+
+				if (tim.length != 0 && mini.length != 0 && meri.length != 0) {
+
+                    // if the seconds are not available..
+				    if (!settings.show_seconds || secs.length == 0) {
+				        secs = "00";
+				    } 
+
+					// store the value so we can set the initial value
+					// next time the picker is opened
+					ele.attr('data-timepicki-tim', tim);
+					ele.attr('data-timepicki-mini', mini);
+					ele.attr('data-timepicki-secs', secs);
+					ele.attr('data-timepicki-meri', meri);
+
+					// set the formatted value
+					ele.val(settings.format_output(tim, mini, secs, meri, settings.show_seconds));
+				}
+
+				if (close) {
+				    //console.info("closing timepicker");
+					close_timepicki();
+				}
+			}
+
+			function open_timepicki(event) {
+			    //console.log("opening timepicker");
+			    var input_time;
+
+			    if (settings.start_time_origin) {
+					switch(settings.start_time_origin) {
+						case 'control':
+						    input_time = settings.format_input($(event.currentTarget).val());
+							break;
+					    case 'current':
+                            // leave it alone.
+							break;
+					    case 'preset':
+					        input_time = settings.preset_start_time;
+							break;
+					    default:
+					        break;
+					}
+				}
+
+				set_date(input_time);
 				ele_next.fadeIn();
 				// focus on the first input and select its contents
 				ele_next.find('input:visible').first().focus().select();
@@ -121,46 +272,45 @@
 			}
 
 			function set_date(start_time) {
+				var d, ti, mi, se, mer;
 
 				// if a value was already picked we will remember that value
 				if (ele.is('[data-timepicki-tim]')) {
-					var ti = Number(ele.attr('data-timepicki-tim'));
-					var mi = Number(ele.attr('data-timepicki-mini'));
-					var mer = ele.attr('data-timepicki-meri');
+					ti = Number(ele.attr('data-timepicki-tim'));
+					mi = Number(ele.attr('data-timepicki-mini'));
+					se = Number(ele.attr('data-timepicki-secs'));
+					mer = ele.attr('data-timepicki-meri');
 
 				// developer can specify a custom starting value
 				} else if (typeof start_time === 'object') {
-					var ti = Number(start_time[0]);
-					var mi = Number(start_time[1]);
-					var mer = start_time[2];
+					ti = Number(start_time[0]);
+					mi = Number(start_time[1]);
+					se = Number(start_time[2]);
+					mer = start_time[3];
 
 				// default is we will use the current time
 				} else {
-					var d = new Date();
-					var ti = d.getHours();
-					var mi = d.getMinutes();
-					var mer = "AM";
+					d = new Date();
+					ti = d.getHours();
+					mi = d.getMinutes();
+					se = d.getSeconds();
+					mer = "AM";
 					if (12 < ti) {
 						ti -= 12;
 						mer = "PM";
 					}
 				}
 
-				if (ti < 10) {
-					ele_next.find(".ti_tx input").val("0" + ti);
-				} else {
-					ele_next.find(".ti_tx input").val(ti);
-				}
-				if (mi < 10) {
-					ele_next.find(".mi_tx input").val("0" + mi);
-				} else {
-					ele_next.find(".mi_tx input").val(mi);
-				}
-				if (mer < 10) {
-					ele_next.find(".mer_tx input").val("0" + mer);
-				} else {
-					ele_next.find(".mer_tx input").val(mer);
-				}
+				ti = (ti < 10) ? "0" + ti : ti;
+				mi = (mi < 10) ? "0" + mi : mi;
+				se = (se < 10) ? "0" + se : se;
+				mer = (mer < 10) ? "0" + mer : mer;
+
+				ele_next.find(".ti_tx").text(ti);
+				ele_next.find(".mi_tx").text(mi);
+			    ele_next.find(".se_tx").text(se);
+				ele_next.find(".mer_tx").text(mer);
+			
 			}
 
 
@@ -168,7 +318,7 @@
 			var cur_prev = ele_next.find(".action-prev");
 
 
-			$(cur_prev).add(cur_next).on("click", function() {
+			$(cur_prev).add(cur_next).click(function(event) {
 				//console.log("click");
 				var cur_ele = $(this);
 				var cur_cli = null;
@@ -179,32 +329,32 @@
 					cur_cli = "time";
 					ele_en = 12;
 					var cur_time = null;
-					cur_time = ele_next.find("." + cur_cli + " .ti_tx input").val();
+					cur_time = ele_next.find("." + cur_cli + " .ti_tx").text();
 					cur_time = Number(cur_time);
 					//console.log(ele_next.find("." + cur_cli + " .ti_tx"));
 					if ($(cur_ele).hasClass('action-next')) {
 						//alert("nex");
 						if (cur_time == 12) {
-							ele_next.find("." + cur_cli + " .ti_tx input").val("01");
+							ele_next.find("." + cur_cli + " .ti_tx ").text("01");
 						} else {
 							cur_time++;
 
 							if (cur_time < 10) {
-								ele_next.find("." + cur_cli + " .ti_tx input").val("0" + cur_time);
+								ele_next.find("." + cur_cli + " .ti_tx").text("0" + cur_time);
 							} else {
-								ele_next.find("." + cur_cli + " .ti_tx input").val(cur_time);
+								ele_next.find("." + cur_cli + " .ti_tx").text(cur_time);
 							}
 						}
 
 					} else {
 						if (cur_time == 1) {
-							ele_next.find("." + cur_cli + " .ti_tx input").val(12);
+							ele_next.find("." + cur_cli + " .ti_tx").text("12");
 						} else {
 							cur_time--;
 							if (cur_time < 10) {
-								ele_next.find("." + cur_cli + " .ti_tx input").val("0" + cur_time);
+								ele_next.find("." + cur_cli + " .ti_tx").text("0" + cur_time);
 							} else {
-								ele_next.find("." + cur_cli + " .ti_tx input").val(cur_time);
+								ele_next.find("." + cur_cli + " .ti_tx").text(cur_time);
 							}
 						}
 					}
@@ -214,59 +364,92 @@
 					cur_cli = "mins";
 					ele_en = 59;
 					var cur_mins = null;
-					cur_mins = ele_next.find("." + cur_cli + " .mi_tx input").val();
+					cur_mins = ele_next.find("." + cur_cli + " .mi_tx").text();
 					cur_mins = parseInt(cur_mins);
 					if ($(cur_ele).hasClass('action-next')) {
 						//alert("nex");
 						if (cur_mins == 59) {
-							ele_next.find("." + cur_cli + " .mi_tx input").val("00");
+							ele_next.find("." + cur_cli + " .mi_tx").text("00");
 						} else {
 							cur_mins++;
 							if (cur_mins < 10) {
-								ele_next.find("." + cur_cli + " .mi_tx input").val("0" + cur_mins);
+								ele_next.find("." + cur_cli + " .mi_tx").text("0" + cur_mins);
 							} else {
-								ele_next.find("." + cur_cli + " .mi_tx input").val(cur_mins);
+								ele_next.find("." + cur_cli + " .mi_tx").text(cur_mins);
 							}
 						}
 					} else {
 
 						if (cur_mins == 0) {
-							ele_next.find("." + cur_cli + " .mi_tx input").val(59);
+							ele_next.find("." + cur_cli + " .mi_tx").text("59");
 						} else {
 							cur_mins--;
 
 							if (cur_mins < 10) {
-								ele_next.find("." + cur_cli + " .mi_tx input").val("0" + cur_mins);
+								ele_next.find("." + cur_cli + " .mi_tx").text("0" + cur_mins);
 							} else {
-								ele_next.find("." + cur_cli + " .mi_tx input").val(cur_mins);
+								ele_next.find("." + cur_cli + " .mi_tx").text(cur_mins);
 							}
 
 						}
 
 					}
+				} else if (cur_ele.parent().attr("class") == "secs") {
+				    //alert("secs");
+				    cur_cli = "secs";
+				    ele_en = 59;
+				    var cur_secs = null;
+				    cur_secs = ele_next.find("." + cur_cli + " .se_tx").text();
+				    cur_secs = parseInt(cur_secs);
+				    if ($(cur_ele).hasClass('action-next')) {
+				        //alert("nex");
+				        if (cur_secs == 59) {
+				            ele_next.find("." + cur_cli + " .se_tx").text("00");
+				        } else {
+				            cur_secs++;
+				            if (cur_secs < 10) {
+				                ele_next.find("." + cur_cli + " .se_tx").text("0" + cur_secs);
+				            } else {
+				                ele_next.find("." + cur_cli + " .se_tx").text(cur_secs);
+				            }
+				        }
+				    } else {
+
+				        if (cur_secs == 0) {
+				            ele_next.find("." + cur_cli + " .se_tx").text("59");
+				        } else {
+				            cur_secs--;
+
+				            if (cur_secs < 10) {
+				                ele_next.find("." + cur_cli + " .se_tx").text("0" + cur_secs);
+				            } else {
+				                ele_next.find("." + cur_cli + " .se_tx").text(cur_secs);
+				            }
+
+				        }
+
+				    }
 				} else {
-					//alert("merdian");
+					//alert("meridian");
 					ele_en = 1;
 					cur_cli = "meridian";
 					var cur_mer = null;
-					cur_mer = ele_next.find("." + cur_cli + " .mer_tx input").val();
+					cur_mer = ele_next.find("." + cur_cli + " .mer_tx").text();
 					if ($(cur_ele).hasClass('action-next')) {
 						//alert(cur_mer);
 						if (cur_mer == "AM") {
-							ele_next.find("." + cur_cli + " .mer_tx input").val("PM");
+							ele_next.find("." + cur_cli + " .mer_tx").text("PM");
 						} else {
-							ele_next.find("." + cur_cli + " .mer_tx input").val("AM");
+							ele_next.find("." + cur_cli + " .mer_tx").text("AM");
 						}
 					} else {
 						if (cur_mer == "AM") {
-							ele_next.find("." + cur_cli + " .mer_tx input").val("PM");
+							ele_next.find("." + cur_cli + " .mer_tx").text("PM");
 						} else {
-							ele_next.find("." + cur_cli + " .mer_tx input").val("AM");
+							ele_next.find("." + cur_cli + " .mer_tx").text("AM");
 						}
 					}
 				}
-
-
 			});
 
 		});
